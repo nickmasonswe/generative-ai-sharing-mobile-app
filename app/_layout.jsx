93a744b4aded4ca089b54b1990d8be9b2c61c55f
nickmasonswe <nickmasonswe@gmail.com>
2024-04-15 14:31:29 -0400

    import and add Slot component from expo-router into _layout. Use Slot to render it's current child route, causing the text from the App function index.jsx that is wrapped in view to be rendered. + removed styling in _layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,22 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import { Slot } from 'expo-router';
 const RootLayout = () => {
   return (
-    <View style={styles.container}>
-      <Text>RootLayout</Text>
-    </View>
+    <>
+    <Text>Header</Text>
+    <Slot />
+    <Text>Footer</Text>
+    </>
+
   )
 }
 
 export default RootLayout
 
-const styles = StyleSheet.create({
-  container : {
-    display: 'flex',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-
-  }
-})
