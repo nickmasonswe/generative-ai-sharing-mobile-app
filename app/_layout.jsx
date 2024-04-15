4188eccf1468cf7c384b73bb8f866d7036d281e0
nickmasonswe <nickmasonswe@gmail.com>
2024-04-15 13:44:55 -0400

    add index file as main page, pasting previous _layout code into it, and solidifying _layout as RootLayout for routing purposes

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-export default function App() {
+const RootLayout = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>RootLayout</Text>
     </View>
-  );
+  )
 }
 
+export default RootLayout
+
 const styles = StyleSheet.create({
-  container: {
+  container : {
+    display: 'flex',
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+
+  }
+})
